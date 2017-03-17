2dfabe74bf392e660a544376bb76759da1f463fb
Golden <joagyl@student.chalmers.se>
2017-03-17 14:32:44 +0100

    Cleaned up the code. Started to implement a high pass filter

--- IMU/IMU.ino
+++ IMU/IMU.ino
@@ -46,48 +46,62 @@ void I2CwriteByte(uint8_t Address, uint8_t Register, uint8_t Data)
 }
 
 // === Acceleration ===
-float accX = 0;
-float accY = 0;
-float accZ = 0;
+float acc[3] = {0,0,0};
+float newAcc[3] = {0,0,0};
 
 // Return acceleration
 float getAccX() {
-  return accX;
+  return acc[0];
 }
 float getAccY() {
-  return accY;
+  return acc[1];
 }
 float getAccZ() {
-  return accZ;
+  return acc[2];
 }
 
 // === Velocity ===
-float velX = 0;
-float velY = 0;
-float velZ = 0;
+float vel[3] = {0,0,0};
 
 // Return velocity
 float getVelX() {
-  return velX;
+  return vel[0];
 }
 float getVelY() {
-  return velY;
+  return vel[1];
 }
 float getVelZ() {
-  return velZ;
+  return vel[2];
 }
 
 // Update velocity
 void setVelX(float v) {
-  velX = v;
+  vel[0] = v;
 }
 void setVelY(float v) {
-  velY = v;
+  vel[1] = v;
 }
 void setVelZ(float v) {
-  velZ = v;
+  vel[2] = v;
 }
 
+float prev[3];
+float prevHF[3];
+boolean doHPF = false;
+
+// ind - index of three dimensions (x, y, z)
+void makeHPFf() {
+    for (int ind = 0; ind < 3; ind++) {
+        newAcc[ind] = acc[ind] * 0.002 * 9.8;
+        if (doHPF)
+            acc[ind] = newAcc[ind] - prev[ind] + (prevHF[ind] * 0.8);
+        prev[ind] = newAcc[ind];
+        prevHF[ind] = acc[ind];
+    }
+
+    if (!doHPF)
+        doHPF = true;
+}
 
 // Initializations
 void setup()
@@ -98,6 +112,7 @@ void setup()
 
   // Configure accelerometers range
     I2CwriteByte(MPU9250_ADDRESS,28,ACC_FULL_SCALE_16_G);
+    
 }
 
 
@@ -130,43 +145,49 @@ void loop()
 
 
   // Convert 16 bits values to m/s^2
-  float axf= ax;
-  float ayf = ay;
-  float azf = az;
+  float af[3] = {ax, ay, az};
+  
   // Sensitiivy scale factor: 2048, g: 9.82 m/s^2
-  accX = axf/2048*9.82;
-  accY = ayf/2048*9.82;
-  accZ = azf/2048*9.82;
-  // acceleration = alpha * readAccelerationSensor() + (1 - alpha) * lastSensorReading
+  acc[0] = af[0]/2048*9.82;
+  acc[1] = af[1]/2048*9.82;
+  acc[2] = af[2]/2048*9.82;
+
+  // Run highpass filter
+  makeHPFf();
 
   // ____________________________________
   // ::: Velocity :::
 
   // Calculate and update velocity
-  float tempVelX = velX;
-  float tempVelY = velY;
-  float tempVelZ = velZ;
+  float tempVel[3] = {vel[0], vel[1], vel[2]};
 
-  velX = tempVelX + (accX*0.01);
-  velY = tempVelY + (accY*0.01);
-  velZ = tempVelZ + (accZ*0.01);
+  vel[0] = tempVel[0] + (acc[0]*0.01);
+  vel[1] = tempVel[1] + (acc[1]*0.01);
+  vel[2] = tempVel[2] + (acc[2]*0.01);
 
   // Display values
 
   // Accelerometer
+  Serial.print ("AccX: ");
   Serial.print (getAccX(),DEC); 
   Serial.print ("\t");
+  Serial.print ("AccY: ");
   Serial.print (getAccY(),DEC);
   Serial.print ("\t");
+  Serial.print ("AccZ: ");
   Serial.print (getAccZ(),DEC);
   Serial.print ("\n");
 
   // Velocity
+  Serial.print ("VelX: ");
   Serial.print (getVelX(),DEC);
   Serial.print ("\t");
+  Serial.print ("VelY: ");
   Serial.print (getVelY(),DEC);
   Serial.print ("\t");
+  Serial.print ("VelZ: ");
   Serial.print (getVelZ(),DEC);
+  Serial.print ("VelZ: ");
 
   // End of line
   Serial.println("");
