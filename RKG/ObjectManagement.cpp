0968c4a8b58bf68a476a4742f375e43bb3bee38c
Golden <joagyl@student.chalmers.se>
2017-04-10 12:10:13 +0200

    Created class used to control RGK

---
+++ RKG/ObjectManagement.cpp
@@ -0,0 +1,73 @@
+//
+//
+//
+
+#include "ObjectManagement.h"
+
+
+// Constructor
+ObjectManagement::ObjectManagement()
+{
+}
+
+
+/*
+ ***** Public methods *****
+ */
+
+// set the average speed with a filter
+void ObjectManagement::setAvgSpeed(float avgspeed)
+{
+  avgspeedgoal = avgspeed;
+}
+
+float ObjectManagement::getAvgSpeed()
+{
+ 
+  if(current_avg < avgspeedgoal || detection){
+    current_avg = current_avg + PERIOD*avgspeedgoal/2; //filter, efter 1 sekund blir värdet riktigt.
+  }
+  return current_avg;
+}
+
+void ObjectManagement::objectDetection()
+{
+  current_avg = 0;
+  avgspeedgoal = -20;
+  turning = false;
+  angleref = 0;
+  time4detection = millis();
+  detection = true;
+}
+
+float ObjectManagement::getAngleRef()
+{
+  if(millis() - time4detection > 4500 && detection){
+    detection = false;
+    current_avg = 0;
+    avgspeedgoal = 0;
+    angleref = random(45,195);
+    turning = true;
+    Serial.print("angleref ");
+    Serial.println(angleref);
+  }
+
+  return angleref;
+}
+
+bool ObjectManagement::turningDone(float angle)
+{
+  if(abs(angleref - angle) < 15 && turning){
+    //Serial.println(angle);
+    turning = false;
+    angleref = 0;
+    avgspeedgoal = 30;
+    return true;
+  }
+  else{ 
+    return false;
+  }
+}
+/*
+ ***** Private methods *****
+ */
---
+++ RKG/ObjectManagement.h
@@ -0,0 +1,43 @@
+/*
+ * 
+ */
+
+#ifndef OBJECT_MANAGEMENT_H
+#define OBJECT_MANAGEMENT_H
+
+/*
+ * Defines
+ */
+#define PERIOD 0.03     // Period in seconds, 0.002 prev, 0.04 for whole program
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+class ObjectManagement {
+public:
+    ObjectManagement();              // Constructor
+    void setAvgSpeed(float Avgspeed);     // 
+    float getAvgSpeed();
+    void objectDetection();
+    float getAngleRef();
+    bool turningDone(float angle);
+    
+    
+
+private:
+    // void privateFunction();
+    float avgspeedgoal;
+    float current_avg = 0;
+    unsigned long time4detection;
+    double h;
+    unsigned long timer;
+    unsigned long last_timer;
+    float angleref = 0;
+    bool detection = false;
+    bool turning = false;
+};
+
+
+#endif // OBJECT_MANAGEMENT_H
